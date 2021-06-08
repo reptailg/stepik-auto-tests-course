659d80f4c80b42a1c5d061988c30adc429784ddf
Dmitriy Perepelitsa <dmitriy.perepelitsa@sibit.tech>
2021-06-08 13:23:19 +0700

    Code changes in test_items.py

--- test_items.py
+++ test_items.py
@@ -1,8 +1,8 @@
-import time
+#import time
 
 def test_language_link(browser):
 
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     browser.find_element_by_css_selector(".btn-add-to-basket")
-    time.sleep(3)
+    #time.sleep(3)
